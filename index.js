b7859d7492efbca3d97567b0a08815b543d88a10
Carlos Marin <33185069+carlosmarin88@users.noreply.github.com>
2024-08-21 12:06:08 -0300

    proyecto backend para eventos app

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+require('dotenv').config();
+
+//console.log(process.env);
+
+// Crear el servidor de express
+const app = express();
+
+
+// Directorio publico
+app.use( express.static('public') );
+
+// Rutas
+//TODO: auth // crear, login, renew
+//TODO: CRUD: Eventos
+
+// Esuchar peticiones
+app.listen(process.env.PORT, ()=>{
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+})
